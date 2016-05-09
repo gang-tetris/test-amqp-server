43f6de89928c35dc5141bffcb8368073a55bbc83
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-09 16:09:46 +0300

    Resolved #1, #3

--- src/client.py
+++ src/client.py
@@ -2,12 +2,13 @@
 import pika
 import uuid
 from sys import argv
+from json import dumps as stringify, loads as jsonify
 
-class FibonacciRpcClient(object):
-    def __init__(self):
+class PersonRpcClient(object):
+    def __init__(self, connection):
+        #self.connection = connection
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(
-                host='localhost'))
-
+                                                  host='rabbit'))
         self.channel = self.connection.channel()
 
         result = self.channel.queue_declare(exclusive=True)
@@ -20,22 +21,28 @@ class FibonacciRpcClient(object):
         if self.corr_id == props.correlation_id:
             self.response = body
 
-    def call(self, n):
+    def call(self, person):
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(exchange='',
-                                   routing_key='rpc_queue',
+                                   routing_key='java_queue',
                                    properties=pika.BasicProperties(
                                          reply_to = self.callback_queue,
                                          correlation_id = self.corr_id,
                                          ),
-                                   body=str(n))
+                                   body=person)
         while self.response is None:
             self.connection.process_data_events()
-        return int(self.response)
+        return self.response
 
-fibonacci_rpc = FibonacciRpcClient()
+if __name__ == '__main__':
+    person_rpc = PersonRpcClient()
 
-print(" [x] Requesting fib({})".format(argv[1]))
-response = fibonacci_rpc.call(argv[1])
-print(" [.] Got %r" % response)
+    print(" [x] Requesting {} person {} ({})".format(argv[1], argv[2], argv[3]))
+    response = person_rpc.call(stringify({
+        'op': argv[1],
+        'name': argv[2],
+        'age': int(argv[3])
+    }))
+    print(" [.] Got %r" % jsonify(response.decode("utf-8")))
+
--- src/server.py
+++ src/server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from pika import BlockingConnection, ConnectionParameters
-from remote_greeting import on_request
+from .remote_greeting import on_request
+from .client import PersonRpcClient
 
 DEFAULT_QUEUE = 'rpc_queue'
 DEFAULT_HOST = 'rabbit'
@@ -12,7 +13,12 @@ channel = connection.channel()
 channel.queue_declare(queue=DEFAULT_QUEUE)
 
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume(on_request, queue=DEFAULT_QUEUE)
+
+client = PersonRpcClient(connection)
+def on_request_wrapper(ch, method, props, body):
+    return on_request(client, ch, method, props, body)
+
+channel.basic_consume(on_request_wrapper, queue=DEFAULT_QUEUE)
 
 print(" [x] Awaiting RPC requests")
 channel.start_consuming()
